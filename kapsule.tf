6995b3b766aa55f7d6bd0993c4aedeafb4543df6
Otmane-Guo <58006515+Otmane-Guo@users.noreply.github.com>
2021-05-20 12:07:42 +0100

    Create kapsule.tf

---
+++ kapsule.tf
@@ -0,0 +1,29 @@
+provider "scaleway" {
+  zone            = "fr-par-1"
+  region          = "fr-par"
+}
+
+resource "scaleway_k8s_cluster_beta" "griloKapsule" {
+  name = "griloKapsule"
+  version = "1.18.2"
+  cni = "weave"
+  enable_dashboard = true
+  ingress = "nginx"
+  default_pool {
+    node_type = "GP1-XS"
+    size = 1
+    autoscaling = true
+    autohealing = true
+    min_size = 1
+    max_size = 3
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content = scaleway_k8s_cluster_beta.griloKapsule.kubeconfig[0].config_file
+  filename = "${path.module}/kubeconfig"
+}
+
+output "cluster_url" {
+  value = scaleway_k8s_cluster_beta.griloKapsule.apiserver_url
+}
