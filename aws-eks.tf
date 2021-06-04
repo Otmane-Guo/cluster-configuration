ad102867485e49a5db4e5a010522a492f564ba1e
Otmane-Guo <58006515+Otmane-Guo@users.noreply.github.com>
2021-06-04 16:00:14 +0100

    Update aws-eks.tf

--- aws-eks.tf
+++ aws-eks.tf
@@ -10,14 +10,14 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  load_config_file       = true
   version                = "~> 1.9"
 }
 
 module "my-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-cluster"
-  cluster_version = "1.20"
+  cluster_version = "1.19"
   subnets         = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
   vpc_id          = "vpc-1234556abcdef"
 
