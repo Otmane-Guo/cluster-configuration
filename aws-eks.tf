79a92d0a7b55e8849032e7715ad79614e09ba2a7
Otmane-Guo <58006515+Otmane-Guo@users.noreply.github.com>
2021-06-04 15:54:36 +0100

    update cluster version

--- aws-eks.tf
+++ aws-eks.tf
@@ -17,7 +17,7 @@ provider "kubernetes" {
 module "my-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "my-cluster"
-  cluster_version = "1.17"
+  cluster_version = "1.20"
   subnets         = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
   vpc_id          = "vpc-1234556abcdef"
 
